16ef22ac0e8596e3972eb7274ab75687080b22f8
BenH14 <psybth@exmail.nottingham.ac.uk>
2019-11-12 13:32:32 -0800

    Added android project

---
+++ Android/app/src/androidTest/java/com/cpen442/gamechangers/doorlockcodegenerator/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.cpen442.gamechangers.doorlockcodegenerator;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.cpen442.gamechangers.doorlockcodegenerator", appContext.getPackageName());
+    }
+}
