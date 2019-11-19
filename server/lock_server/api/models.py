216ae0debbf08c36fd710cd46cd8b19473ad3895
Mike Yue <mikeyue97@gmail.com>
2019-11-18 17:15:16 -0800

    model setup

--- server/lock_server/api/models.py
+++ server/lock_server/api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Lock(models.Model):
 
-# Create your models here.
+    lock_id = models.CharField(
+        max_length=120,
+        unique=True,
+        null=False,
+    )
+
+    users = models.ManyToManyField(
+        User,
+        blank=True
+    )
+
+
+class Code(models.Model):
+
+    code = models.IntegerField()
+
+    lock = models.ForeignKey(
+        Lock,
+        on_delete=models.CASCADE,
+        null=False,
+    )
+
+    expiry_time = models.DateTimeField()
+
+    expired = models.BooleanField(
+        default=False
+    )
--- server/lock_server/api/serializers.py
+++ server/lock_server/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-
+from api.models import Lock, Code
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
