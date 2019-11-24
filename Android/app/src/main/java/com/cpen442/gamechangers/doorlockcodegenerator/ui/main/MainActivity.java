11f91b09c3ea034586ec07f7be91f05165ec2ac3
Ben Henaghan <benjamin.henaghan@gmail.com>
2019-11-24 13:03:24 -0800

    Initial structure for main activity

--- Android/app/src/main/java/com/cpen442/gamechangers/doorlockcodegenerator/ui/main/MainActivity.java
+++ Android/app/src/main/java/com/cpen442/gamechangers/doorlockcodegenerator/ui/main/MainActivity.java
@@ -1,56 +1,76 @@
 package com.cpen442.gamechangers.doorlockcodegenerator.ui.main;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.graphics.Rect;
 import android.os.Bundle;
-import android.view.View;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.cpen442.gamechangers.doorlockcodegenerator.R;
-import com.cpen442.gamechangers.doorlockcodegenerator.data.model.Lock;
 
-import java.util.ArrayList;
-import java.util.List;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter lockListAdapter;
-    private RecyclerView.LayoutManager layoutManager;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.code_label)
+    TextView mCodeText;
+
+    @BindView(R.id.time_remaining_label)
+    TextView mTimeRemainingText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.lockList);
-        recyclerView.setHasFixedSize(true);
-        // Linear layout manager
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        ButterKnife.bind(this);
 
-        // Add space between items
-        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+        setSupportActionBar(mToolbar);
+
+
+    }
+
+    /**
+     *
+     * @param duration duration in ms
+     */
+    private void countDownLock(long duration) {
+        new CountDownTimer(duration, 1000) {
+
             @Override
-            public void getItemOffsets(@NonNull Rect outRect, @NonNull View view,
-                                       @NonNull RecyclerView parent,
-                                       @NonNull RecyclerView.State state) {
-                super.getItemOffsets(outRect, view, parent, state);
-                outRect.bottom = 30;
+            public void onTick(long msRemaining) {
+                long seconds = msRemaining / 1000;
+                long minutes = seconds / 60;
+                if (minutes < 1) {
+                    mTimeRemainingText.setText(seconds + "seconds remaining.");
+                } else {
+                    mTimeRemainingText.setText(minutes + "minutes remaining.");
+                }
             }
-        });
 
-        // specify an adapter
-        List<Lock> locks = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            Lock lock = new Lock();
-            lock.setDisplay_name("test" + i);
-            locks.add(lock);
-        }
-        lockListAdapter = new LockListAdapter(locks);
-        recyclerView.setAdapter(lockListAdapter);
+            @Override
+            public void onFinish() {
+                mTimeRemainingText.setText(R.string.time_remaining_idle_text);
+            }
+        };
+    }
+
+
+
+    @OnClick(R.id.unlock_now_button)
+    void generateOwnCode() {
+
+    }
+
+    @OnClick(R.id.guest_code_button)
+    void generateGuestCode() {
+
     }
 }
